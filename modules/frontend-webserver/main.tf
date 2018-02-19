fb4bf8defac768f70ab4cba739705614e5b28fa2
oedm <git@moed.cc>
2018-02-19 12:00:39 +0100

    Remove AZs from ELB

--- modules/frontend-webserver/main.tf
+++ modules/frontend-webserver/main.tf
@@ -95,7 +95,6 @@ data "aws_availability_zones" "all" {}
 
 resource "aws_elb" "frontend_webserver_elb" {
   name                        = "frontend-webserver-elb"
-  availability_zones          = ["${data.aws_availability_zones.all.names}"]
   security_groups             = ["${aws_security_group.frontend-webserver_sg.id}"]
   subnets                     = ["${var.vpc_zone_identifier}"]
   cross_zone_load_balancing   = true
